bb646c6439e99df6ccc68c87ab6fbdce81de1cb8
ppulido <pulidoibarra15@gmail.com>
2025-06-21 15:47:25 -0500

    projecto menu santorini

---
+++ static/js/product.js
@@ -0,0 +1,52 @@
+
+export const productos = [
+    {
+        nombre: "Cerveza Corona",
+        precio: 12,
+        categoria: "cervezas",
+        imagen: "/static/img/corona.jpg"
+    },
+    {
+        nombre: "Mojito Clásico",
+        precio: 18,
+        categoria: "cocteles",
+        imagen: "/static/img/mojito.jpg"
+    },
+    {
+        nombre: "Piña Colada",
+        precio: 20,
+        precioAntiguo: 24,
+        categoria: "cocteles",
+        imagen: "/static/img/piña_colada.jpg"
+    },
+    {
+        nombre: "Agua con Gas",
+        precio: 6,
+        categoria: "sin_alcohol",
+        imagen: "/static/img/agua_gas.jpg"
+    },
+    {
+        nombre: "Coca-Cola",
+        precio: 7,
+        categoria: "sin_alcohol",
+        imagen: "/static/img/coca-cola.jpg"
+    },
+    {
+        nombre: "Whisky Jack Daniel's",
+        precio: 28,
+        categoria: "shots",
+        imagen: "/static/img/whisky_jack_daniels.jpg"
+    },
+    {
+        nombre: "Tequila",
+        precio: 30,
+        categoria: "shots",
+        imagen: "/static/img/tequila.jpg"
+    },
+    {
+        nombre: "Mani",
+        precio: 15,
+        categoria: "snacks",
+        imagen: "https://megatiendas.vtexassets.com/arquivos/ids/168557/7702409012020.png?v=638494088510100000"
+    }
+];
---
+++ static/js/script.js
@@ -0,0 +1,65 @@
+// app.js
+import { productos } from './product.js';
+
+const menuContainer = document.getElementById("menu-container");
+
+function mostrarProductos(filtro = "Todos") {
+  menuContainer.innerHTML = "";
+
+  const filtrados = filtro === "Todos"
+    ? productos
+    : productos.filter(p => p.categoria === filtro);
+
+  filtrados.forEach(prod => {
+    const producto = document.createElement("div");
+    producto.classList.add("producto");
+
+    producto.innerHTML = `
+      <img src="${prod.imagen}" alt="${prod.nombre}" />
+      <div>
+        <div class="nombre">${prod.nombre}</div>
+        <div class="precio">
+          $${prod.precio}
+        </div>
+      </div>
+    `;
+
+    menuContainer.appendChild(producto);
+  });
+}
+
+mostrarProductos();
+
+document.querySelectorAll("[data-categoria]").forEach(btn => {
+  btn.addEventListener("click", e => {
+    e.preventDefault();
+    const categoria = btn.getAttribute("data-categoria");
+
+    document.querySelectorAll("#category-nav .nav-link").forEach(el => el.classList.remove("active"));
+    if (btn.classList.contains("nav-link")) {
+      btn.classList.add("active");
+    }
+
+    mostrarProductos(categoria);
+    cerrarMenu();
+  });
+});
+
+// Menú lateral
+const menuBtn = document.getElementById("menu-toggle");
+const sideMenu = document.getElementById("side-menu");
+const overlay = document.getElementById("overlay");
+const closeBtn = document.getElementById("close-menu");
+
+menuBtn.addEventListener("click", () => {
+  sideMenu.style.left = "0";
+  overlay.style.display = "block";
+});
+
+overlay.addEventListener("click", cerrarMenu);
+closeBtn.addEventListener("click", cerrarMenu);
+
+function cerrarMenu() {
+  sideMenu.style.left = "-250px";
+  overlay.style.display = "none";
+}
