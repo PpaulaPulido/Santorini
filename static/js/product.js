524f426bdef7349173b76d21ed1db045b759401e
ppulido <pulidoibarra15@gmail.com>
2025-06-25 12:53:39 -0500

    img

--- static/js/product.js
+++ static/js/product.js
@@ -1,52 +1,74 @@
-
 export const productos = [
-    {
-        nombre: "Cerveza Corona",
-        precio: 12,
-        categoria: "cervezas",
-        imagen: "/static/img/corona.jpg"
-    },
-    {
-        nombre: "Mojito Clásico",
-        precio: 18,
-        categoria: "cocteles",
-        imagen: "/static/img/mojito.jpg"
-    },
-    {
-        nombre: "Piña Colada",
-        precio: 20,
-        precioAntiguo: 24,
-        categoria: "cocteles",
-        imagen: "/static/img/piña_colada.jpg"
-    },
-    {
-        nombre: "Agua con Gas",
-        precio: 6,
-        categoria: "sin_alcohol",
-        imagen: "/static/img/agua_gas.jpg"
-    },
-    {
-        nombre: "Coca-Cola",
-        precio: 7,
-        categoria: "sin_alcohol",
-        imagen: "/static/img/coca-cola.jpg"
-    },
-    {
-        nombre: "Whisky Jack Daniel's",
-        precio: 28,
-        categoria: "shots",
-        imagen: "/static/img/whisky_jack_daniels.jpg"
-    },
-    {
-        nombre: "Tequila",
-        precio: 30,
-        categoria: "shots",
-        imagen: "/static/img/tequila.jpg"
-    },
-    {
-        nombre: "Mani",
-        precio: 15,
-        categoria: "snacks",
-        imagen: "https://megatiendas.vtexassets.com/arquivos/ids/168557/7702409012020.png?v=638494088510100000"
-    }
+    // CERVEZAS
+    { nombre: "Cerveza Pilsen", precio: 6000, categoria: "cervezas", imagen: "/static/img/pilsen.png" },
+    { nombre: "Cerveza Águila", precio: 6000, categoria: "cervezas", imagen: "/static/img/Aguila.jpg" },
+    { nombre: "Cerveza Águila Light", precio: 6000, categoria: "cervezas", imagen: "/static/img/aguila_light.png" },
+    { nombre: "Cerveza Club Dorada", precio: 7000, categoria: "cervezas", imagen: "/static/img/dorada1.png" },
+    { nombre: "Cerveza Corona", precio: 10000, categoria: "cervezas", imagen: "/static/img/corona.jpg" },
+
+    // CÓCTELES
+    { nombre: "Piña Colada", precio: 20000, categoria: "cocteles", imagen: "/static/img/piña_colada.jpg" },
+
+    // SIN ALCOHOL
+    { nombre: "Coca-Cola", precio: 5000, categoria: "sin_alcohol", imagen: "/static/img/coca-cola.jpg" },
+    { nombre: "Pepsi", precio: 5000, categoria: "sin_alcohol", imagen: "/static/img/pepsi.jpg" },
+    { nombre: "7UP", precio: 5000, categoria: "sin_alcohol", imagen: "/static/img/7up.jpg" },
+    { nombre: "Gatorade", precio: 10000, categoria: "sin_alcohol", imagen: "/static/img/gatorade.png" },
+    { nombre: "Hydra Tao", precio: 12000, categoria: "sin_alcohol", imagen: "/static/img/Hidra_Tao.png" },
+    { nombre: "Electrolit", precio: 15000, categoria: "sin_alcohol", imagen: "/static/img/electrolit.jpg" },
+
+    // SODAS
+    { nombre: "Soda", precio: 5000, categoria: "sodas", imagen: "/static/img/soda.png" },
+    { nombre: "Soda Michelada", precio: 7000, categoria: "sodas", imagen: "/static/img/soda_michelada.png" },
+    { nombre: "Saborizada Frutos Rojos", precio: 12000, categoria: "sodas", imagen: "/static/img/frutos_rojos.png" },
+    { nombre: "Saborizada Maracuyá", precio: 12000, categoria: "sodas", imagen: "/static/img/saborisada_maracuya.png" },
+    { nombre: "Saborizada Mango", precio: 12000, categoria: "sodas", imagen: "/static/img/saborisada_mango.jpg" },
+
+    // AGUARDIENTE
+    { nombre: "Trago Aguardiente Sencillo", precio: 7000, categoria: "aguardiente", imagen: "/static/img/aguardiente.jpg" },
+    { nombre: "Trago Aguardiente Doble", precio: 12000, categoria: "aguardiente", imagen: "/static/img/aguardiente.jpg" },
+    { nombre: "Botella Aguardiente Real", precio: 95000, categoria: "aguardiente", imagen: "/static/img/aguardiente_real.jpg" },
+
+    // RON
+    { nombre: "Trago Ron Sencillo", precio: 8000, categoria: "ron", imagen: "/static/img/ron.jpg" },
+    { nombre: "Trago Ron Doble", precio: 13000, categoria: "ron", imagen: "/static/img/ron.jpg" },
+    { nombre: "Ron Juan de la Cruz Media", precio: 75000, categoria: "ron", imagen: "/static/img/Ron Juan de la Cruz Media.png" },
+    { nombre: "Ron Juan de la Cruz Botella", precio: 130000, categoria: "ron", imagen: "/static/img/Ron Juan de la Cruz Botella.png" },
+    { nombre: "Ron Carta Oro 8 años Media", precio: 95000, categoria: "ron", imagen: "/static/img/Ron Carta Oro 8 años Media.png" },
+    { nombre: "Ron Carta Oro 8 años Botella", precio: 170000, categoria: "ron", imagen: "/static/img/Ron Carta Oro 8 años Botella.png" },
+    { nombre: "Ron Importado", precio: 0, categoria: "ron", imagen: "/static/img/ron_importado.jpg" },
+
+    // WHISKY
+    { nombre: "Trago Whisky Sencillo", precio: 20000, categoria: "whisky", imagen: "/static/img/whisky.jpg" },
+    { nombre: "Trago Whisky Doble", precio: 30000, categoria: "whisky", imagen: "/static/img/whisky.jpg" },
+    { nombre: "Whisky Buchanan’s 12 años Media", precio: 180000, categoria: "whisky", imagen: "/static/img/Whisky Buchanan’s 12 años Media.png" },
+    { nombre: "Whisky Buchanan’s 12 años Botella", precio: 250000, categoria: "whisky", imagen: "/static/img/Whisky Buchanan’s 12 años Botella.png" },
+    { nombre: "Whisky Buchanan’s Masters", precio: 300000, categoria: "whisky", imagen: "/static/img/Whisky Buchanan’s Masters.jpg" },
+    { nombre: "Whisky Old Parr 12 años Media", precio: 195000, categoria: "whisky", imagen: "/static/img/Whisky Old Parr 12 años Media.png" },
+    { nombre: "Whisky Old Parr 12 años Botella", precio: 250000, categoria: "whisky", imagen: "/static/img/Whisky Old Parr 12 años Botella.png" },
+    { nombre: "Whisky Old Parr Tribute", precio: 270000, categoria: "whisky", imagen: "/static/img/Whisky Old Parr Tribute.png" },
+    { nombre: "Whisky Old Parr Superior", precio: 290000, categoria: "whisky", imagen: "/static/img/Whisky Old Parr Superior.png" },
+
+    // TEQUILA
+    { nombre: "Tequila Jose Cuervo Especial Media", precio: 95000, categoria: "tequila", imagen: "/static/img/Tequila Jose Cuervo Especial Media.png" },
+    { nombre: "Tequila Jose Cuervo Especial Botella", precio: 155000, categoria: "tequila", imagen: "/static/img/Tequila Jose Cuervo Especial Botella.png" },
+    { nombre: "Tequila Jose Cuervo Silver", precio: 155000, categoria: "tequila", imagen: "/static/img/Tequila Jose Cuervo Silver.png" },
+    { nombre: "Tequila Jose Cuervo Tradicional", precio: 190000, categoria: "tequila", imagen: "/static/img/Tequila Jose Cuervo Tradicional.png" },
+
+    // VODKA
+    { nombre: "Vodka Smirnoff Trago", precio: 15000, categoria: "vodka", imagen: "/static/img/Vodka Smirnoff Trago.jpg" },
+    { nombre: "Vodka Smirnoff Lulo Media", precio: 60000, categoria: "vodka", imagen: "/static/img/Vodka Smirnoff Lulo media.png" },
+    { nombre: "Vodka Smirnoff Lulo Botella", precio: 95000, categoria: "vodka", imagen: "/static/img/Vodka Smirnoff Lulo Botella.png" },
+    { nombre: "Vodka Smirnoff Lulo Litro", precio: 120000, categoria: "vodka", imagen: "/static/img/Vodka Smirnoff Lulo Litro.png" },
+    { nombre: "Vodka Smirnoff Media", precio: 110000, categoria: "vodka", imagen: "/static/img/Vodka Smirnoff media.png" },
+    { nombre: "Vodka Smirnoff Botella", precio: 160000, categoria: "vodka", imagen: "/static/img/Vodka Smirnoff Botella.png" },
+    { nombre: "Vodka Absolut Media", precio: 100000, categoria: "vodka", imagen: "/static/img/Vodka_Absolut_media.png" },
+    { nombre: "Vodka Absolut Botella", precio: 130000, categoria: "vodka", imagen: "/static/img/Vodka_Absolut_Botella.png" },
+    { nombre: "Vodka Absolut Litro", precio: 180000, categoria: "vodka", imagen: "/static/img/Vodka_Absolut.png" },
+
+    // OTROS
+    { nombre: "Crema de Whisky", precio: "", categoria: "otros", imagen: "/static/img/crema_whisky.png" },
+    { nombre: "Crema de Ron", precio: "", categoria: "otros", imagen: "/static/img/crema_ron.png" },
+    { nombre: "Vino", precio: "", categoria: "otros", imagen: "/static/img/vino.png" },
+    { nombre: "Brandy", precio: "", categoria: "otros", imagen: "/static/img/brandy.png" }
 ];
